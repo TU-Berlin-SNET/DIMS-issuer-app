de74417c723e4bb73f0dac5cb68300e7584f8d73
Ömer Ilhan <malik@mailbox.tu-berlin.de>
2019-11-28 15:35:28 +0100

    add configurability through environment variables

--- src/Constants.js
+++ src/Constants.js
@@ -1,5 +1,7 @@
-export const apiBaseUrl = ""REPLACE"";
-export const mongoDBBaseUrl = "http://"REPLACE":8011/";
-//export const apiBaseUrl = "http://localhost:8000/api/"
-//export const mongoDBBaseUrl = "http://localhost:8011/";
-//export const apiBaseUrl = ""REPLACE"";
+const APP_DOMAIN_PROTOCOL = process.env.IDC_API_DOMAIN_PROTOCOL || 'http';
+const APP_DOMAIN_HOST = process.env.IDC_API_DOMAIN_HOST || ""REPLACE"";
+const APP_DOMAIN_PORT = process.env.IDC_API_DOMAIN_PORT || 8000;
+const APP_DOMAIN_ENDPOINT = `${APP_DOMAIN_PROTOCOL}://${APP_DOMAIN_HOST}:${APP_DOMAIN_PORT}`;
+
+export const apiBaseUrl = APP_DOMAIN_ENDPOINT + "/api/";
+export const mongoDBBaseUrl = (process.env.DIMS_DEMO_MONGO_URL || "http://"REPLACE":8011") + '/';
