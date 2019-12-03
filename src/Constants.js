a3f513d5bb0f8035219cc347dfa6af49231d9e14
Ömer Ilhan <malik@mailbox.tu-berlin.de>
2019-12-03 18:25:22 +0100

    add REACT_APP_ prefix to environment variables so they get picked up

--- src/Constants.js
+++ src/Constants.js
@@ -1,7 +1,7 @@
-const APP_DOMAIN_PROTOCOL = process.env.IDC_API_DOMAIN_PROTOCOL || 'http';
-const APP_DOMAIN_HOST = process.env.IDC_API_DOMAIN_HOST || ""REPLACE"";
-const APP_DOMAIN_PORT = process.env.IDC_API_DOMAIN_PORT || 8000;
+const APP_DOMAIN_PROTOCOL = process.env.REACT_APP_IDC_API_DOMAIN_PROTOCOL || 'http';
+const APP_DOMAIN_HOST = process.env.REACT_APP_IDC_API_DOMAIN_HOST || ""REPLACE"";
+const APP_DOMAIN_PORT = process.env.REACT_APP_IDC_API_DOMAIN_PORT || 8000;
 const APP_DOMAIN_ENDPOINT = `${APP_DOMAIN_PROTOCOL}://${APP_DOMAIN_HOST}:${APP_DOMAIN_PORT}`;
 
 export const apiBaseUrl = APP_DOMAIN_ENDPOINT + "/api/";
-export const mongoDBBaseUrl = (process.env.DIMS_DEMO_MONGO_URL || "http://"REPLACE":8011") + '/';
+export const mongoDBBaseUrl = (process.env.REACT_APP_DIMS_DEMO_MONGO_URL || "http://"REPLACE":8011") + '/';
